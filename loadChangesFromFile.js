2aa85012c5b3dffe3860cf4122d3d1f30a035bf5
Edvid <spacemastr@gmail.com>
2022-03-01 15:52:10 +0100

    First Draft of changing commands Includes a test file in "Initial State", and output visible in the browser's console window. Only Switching nations and normal commands are written thus far.
    Report me issues you find, I have not found any yet.

---
+++ loadChangesFromFile.js
@@ -0,0 +1,106 @@
+function loadChangesFromFile(event){
+    var file = event.target.files[0];
+    var reader = new FileReader();
+    let changes; 
+    reader.onload = function(e){
+        changes = e.target.result.split("\r\n");
+        let changedSheets = [];
+        for (let i = 0; i < sheets.length; i++) {
+            const sheet = sheets[i];
+            changedSheets[i] = sheet.split("\r\n");
+            for (let j = 0; j < changedSheets[i].length; j++) {
+                const changedSheet = changedSheets[i][j];
+                changedSheets[i][j] = changedSheet.split("\t");
+            }
+        }
+        const commandRegex = /(?<Operand>([a-z]+)( |\t)|(\+|\=|\-)( |\t)?)(?<Amount>(\d|\.)+)( |\t)(?<Stat_Name>[a-z]+)/gi;
+
+        let currentNationID;
+        for (let i = 0; i < changes.length; i++) {
+            const changeCommand = changes[i];
+            
+            //comment
+            const cc = changeCommand.toLowerCase();
+            
+            if(cc[0] == '#' || cc.length == 0){
+                continue;
+            }
+            //update
+            else if(cc.includes("update")){
+                update();
+            }
+            //nation Switch
+            else if(cc[0] == '>'){
+                let currentNation = cc.substring(1).trim();
+                let found = false;
+                for (let j = 0; j < changedSheets.length; j++) {
+                    const changedSheet = changedSheets[j];
+                    //if Column A, row 2 starts with a '=', look for nation names in another sheet
+                    if(changedSheet[1][0].startsWith("=")) continue;
+                    for (let k = 0; k < changedSheet.length; k++) {
+                        const NationCell = changedSheet[k][0];
+                        if(NationCell.toLowerCase().trim() == currentNation){
+                            currentNationID = k;
+                            found = true;
+                            break;
+                        }
+                    }
+                    if(found == true) break;
+                }
+                if(found == false) alert("A nation name was not written correctly in change commands file: " + currentNation);
+            }
+            //normal commands
+            else{
+                let match = commandRegex.exec(cc);
+                commandRegex.test(cc); // DO NOT ASK ME WHY IS IS NECESSARY. The match sometimes just turns out null if not tested first.
+                const commandParameters = [];
+                commandParameters[0] = match.groups.Operand;
+                commandParameters[1] = match.groups.Amount;
+                commandParameters[2] = match.groups.Stat_Name;
+
+                let found = false;
+                for (let j = 0; j < changedSheets.length; j++) {
+                    const changedSheet = changedSheets[j];
+                    for (let k = 0; k < changedSheet[0].length; k++) {
+                        const StatCell = changedSheet[0][k];
+                        if(StatCell.toLowerCase().trim() == commandParameters[2]){
+                            //if the stat in question isn't even a number, throw an error too
+                            if(changedSheet[currentNationID][k].toString().startsWith("="))
+                            {
+                                alert("A stat that is described as a formula has been attempted changed: " + commandParameters[2] + ".\r\n Action has been aborted.");
+                            } 
+                            //changes
+                            else{
+                                if(commandParameters[0] == '+' || commandParameters[0] == 'add'){
+                                    changedSheet[currentNationID][k] = +changedSheet[currentNationID][k] + +commandParameters[1];
+                                }else if(commandParameters[0] == '-' || commandParameters[0] == 'sub'){
+                                    changedSheet[currentNationID][k] = +changedSheet[currentNationID][k] - +commandParameters[1];
+                                }else if(commandParameters[0] == '=' || commandParameters[0] == 'set'){
+                                    changedSheet[currentNationID][k] = commandParameters[1];
+                                }else{
+                                    alert("Function wasn't understood: " + commandParameters[0] + ".\r\n Aborting.");
+                                }
+                            }
+                            found = true;
+                            break;
+                        }
+                    }
+                    if(found == true) break;
+                }
+                if(found == false) alert("A stat name was not written correctly in change commands file: " + commandParameters[2]);
+            }
+            /* alert("notice me baka"); */
+        }
+
+        changedSheets.forEach(changedSheet => {
+            console.table(changedSheet); 
+        }); 
+    };
+
+    reader.readAsText(file);
+       
+}
+
+function update(){
+    //not implemented yet
+}
