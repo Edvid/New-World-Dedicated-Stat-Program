a2a2bc9e371864c25fb47e60ccdf7962a44880f3
Edvid <spacemastr@gmail.com>
2022-03-16 22:45:18 +0100

    NationSheetGenerator moved over to seperate file

---
+++ NationSheetGenerator.js
@@ -0,0 +1,48 @@
+let primaryColor = ["red", "green", "DodgerBlue", "purple", "Dark Orange"];
+let secondaryColor = ["pink", "light green", "baby blue", "magenta", "Orange"];
+        
+
+function createNationSheet(row){
+    let NationSheetDiv = document.querySelector("div#NationSheet");
+
+    let nationName = document.createElement("h1");
+    nationName.innerHTML = changedSheetsEvaluated["Daily Stuff"][row][0]; 
+    NationSheetDiv.appendChild(nationName);
+    for (let sheetIndex = 0; sheetIndex < sheets.length; sheetIndex++) {
+        if(sheetNames[sheetIndex] == "Trade Zone Wealth") continue;
+        const sheetRows = changedSheetsEvaluated[sheetNames[sheetIndex]];
+        let nationTable = document.createElement("table");
+        nationTable.style.borderCollapse = "collapse";
+        let nationTableTitle = document.createElement("h2");
+        nationTableTitle.innerHTML = sheetNames[sheetIndex];
+        let StatNames = document.createElement("tr");
+        let StatCells = document.createElement("tr");
+        for (let column = 0; column < sheetRows[0].length; column++) {
+            const StatNameText = sheetRows[0][column];
+            const StatCellText = sheetRows[row][column];
+            
+            StatName = document.createElement("th");
+            StatName.style.border = "2px solid #444"
+            StatName.style.backgroundColor = primaryColor[row % primaryColor.length];
+            StatName.innerHTML = StatNameText;
+            StatNames.appendChild(StatName);
+            StatCell = document.createElement("td");
+            StatCell.style.border = "2px solid #444"
+            StatCell.innerHTML = StatCellText;
+            if(sheetRows[row][column].address !== undefined){
+                let StatErrorAddress = document.createElement("span");
+                StatErrorAddress.innerHTML = sheetRows[row][column].address;
+                StatCell.style.color = "red";
+                let br = document.createElement("br");
+                StatCell.appendChild(br);
+                StatCell.appendChild(StatErrorAddress);
+            }
+            StatCells.appendChild(StatCell);
+            
+        }
+        nationTable.appendChild(StatNames);
+        nationTable.appendChild(StatCells);
+        NationSheetDiv.appendChild(nationTableTitle);
+        NationSheetDiv.appendChild(nationTable);
+    }
+}
--- loadChangesFromFile.js
+++ loadChangesFromFile.js
@@ -134,60 +134,8 @@ function loadChangesFromFile(event){
         }
 
         // Parse to evaluated sheets
-
-        primaryColor = ["red", "green", "blue", "purple", "Dark Orange"];
-        secondaryColor = ["pink", "light green", "baby blue", "magenta", "Orange"];
-        
-        let fewestRowsinSheets = 10000;
-        for (const [key, value] of Object.entries(changedSheetsEvaluated)){
-            if(fewestRowsinSheets > value.length && key != "Trade Zone Wealth") fewestRowsinSheets = value.length;
-        }
-
-        console.log(fewestRowsinSheets);
-        
-        for (let row = 1; row < fewestRowsinSheets; row++) {
-            let nationName = document.createElement("h1");
-            nationName.innerHTML = changedSheetsEvaluated["Daily Stuff"][row][0]; 
-            document.body.appendChild(nationName);
-            for (let sheetIndex = 0; sheetIndex < sheets.length; sheetIndex++) {
-                if(sheetNames[sheetIndex] == "Trade Zone Wealth") continue;
-                const sheetRows = changedSheetsEvaluated[sheetNames[sheetIndex]];
-                let nationTable = document.createElement("table");
-                nationTable.style.borderCollapse = "collapse";
-                let nationTableTitle = document.createElement("h2");
-                nationTableTitle.innerHTML = sheetNames[sheetIndex];
-                let StatNames = document.createElement("tr");
-                let StatCells = document.createElement("tr");
-                for (let column = 0; column < sheetRows[0].length; column++) {
-                    const StatNameText = sheetRows[0][column];
-                    const StatCellText = sheetRows[row][column];
-                    
-                    StatName = document.createElement("th");
-                    StatName.style.border = "2px solid #444"
-                    StatName.style.backgroundColor = primaryColor[row % primaryColor.length];
-                    StatName.innerHTML = StatNameText;
-                    StatNames.appendChild(StatName);
-                    StatCell = document.createElement("td");
-                    StatCell.style.border = "2px solid #444"
-                    StatCell.innerHTML = StatCellText;
-                    if(sheetRows[row][column].address !== undefined){
-                        let StatErrorAddress = document.createElement("span");
-                        StatErrorAddress.innerHTML = sheetRows[row][column].address;
-                        StatCell.style.color = "red";
-                        let br = document.createElement("br");
-                        StatCell.appendChild(br);
-                        StatCell.appendChild(StatErrorAddress);
-                    }
-                    StatCells.appendChild(StatCell);
-                    
-                }
-                nationTable.appendChild(StatNames);
-                nationTable.appendChild(StatCells);
-                document.body.appendChild(nationTableTitle);
-                document.body.appendChild(nationTable);
-            }
-            
-        }
+        createNationSheet(2);
+        console.table(changedSheets[0]);
 
     };
 
