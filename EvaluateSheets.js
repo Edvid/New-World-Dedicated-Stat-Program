d5e8935c4a86d7577c1e42374c5079d554a97e5f
Edvid <spacemastr@gmail.com>
2022-03-17 01:52:21 +0100

    numbers that are just numbers, will have their commas in them removed

--- EvaluateSheets.js
+++ EvaluateSheets.js
@@ -7,6 +7,7 @@ const options = {
 let changedSheetsEvaluated = [];
 
 function evaluateSheets(){
+  //convert to hyperformula format
   const hfInstance = HyperFormula.buildEmpty(options);
   for(let i = 0; i < sheetNames.length; i++){
     let sheetI = hfInstance.getSheetId(hfInstance.addSheet(sheetNames[i]));
@@ -16,6 +17,22 @@ function evaluateSheets(){
 }
 
 function readySheets(){
+
+  //make all things that can be rendered as numbers, as numbers
+
+  for (let s = 0; s < sheets.length; s++) {
+    const sheet = sheets[s];
+    for (let r = 0; r < sheet.length; r++) {
+      const row = sheet[r];
+      for (let c = 0; c < row.length; c++) {
+        const cell = row[c];
+        const cellWithoutCommas = cell.replace(",", "");
+        if(!isNaN(cellWithoutCommas)) sheets[s][r][c] = cellWithoutCommas;  
+      }
+    }
+  }
+
+  //convert to hyperformula format
   const hfInstance = HyperFormula.buildEmpty(options);
   for(let i = 0; i < sheetNames.length; i++){
     let sheetI = hfInstance.getSheetId(hfInstance.addSheet(sheetNames[i]));
