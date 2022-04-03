e77ee49815a5cb14db39dbcff08182f768b580c9
Edvid <spacemastr@gmail.com>
2022-04-03 16:07:17 +0200

    Fix recruitment handling

--- specialOperations.js
+++ specialOperations.js
@@ -1,22 +1,25 @@
 function specialOperation(selection, change){
-    
+
     //New Recruitment handling
     if(
         /gameStats\.Nations\..+\.Levies/.test(selection) ||
-        /gameStats\.Nations\..+\.Light Infantry/.test(selection) ||
-        /gameStats\.Nations\..+\.Heavy Infantry/.test(selection) ||
+        /gameStats\.Nations\..+\.LightInfantry/.test(selection) ||
+        /gameStats\.Nations\..+\.HeavyInfantry/.test(selection) ||
         /gameStats\.Nations\..+\.Archers/.test(selection) ||
         /gameStats\.Nations\..+\.Crossbowmen/.test(selection) ||
-        /gameStats\.Nations\..+\.Light Cavalry/.test(selection) ||
-        /gameStats\.Nations\..+\.Heavy Cavalry/.test(selection) ||
-        /gameStats\.Nations\..+\.Elite Infantry/.test(selection) ||
-        /gameStats\.Nations\..+\.Elite Cavalry/.test(selection) ||
-        /gameStats\.Nations\..+\.Hand Cannon/.test(selection) ||
+        /gameStats\.Nations\..+\.LightCavalry/.test(selection) ||
+        /gameStats\.Nations\..+\.HeavyCavalry/.test(selection) ||
+        /gameStats\.Nations\..+\.EliteInfantry/.test(selection) ||
+        /gameStats\.Nations\..+\.EliteCavalry/.test(selection) ||
+        /gameStats\.Nations\..+\.HandCannon/.test(selection) ||
         /gameStats\.Nations\..+\.Musketeers/.test(selection) ||
         /gameStats\.Nations\..+\.Militia/.test(selection) ||
-        /gameStats\.Nations\..+\.Siege Equipment/.test(selection) ||
-        /gameStats\.Nations\..+\.Large Siege Equipment/.test(selection) ||
-        /gameStats\.Nations\..+\.Cannons/.test(selection)
+        /gameStats\.Nations\..+\.SiegeEquipment/.test(selection) ||
+        /gameStats\.Nations\..+\.LargeSiege Equipment/.test(selection) ||
+        /gameStats\.Nations\..+\.Cannons/.test(selection) ||
+        /gameStats\.Nations\..+\.LightShips/.test(selection) ||
+        /gameStats\.Nations\..+\.MediumShips/.test(selection) ||
+        /gameStats\.Nations\..+\.HeavyShips/.test(selection)
     ){
         //only record positive changes
         if(change <= 0) return;
