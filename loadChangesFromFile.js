5b1ecc42d839282f549b5739d0f02276e2bb0446
Edvid <spacemastr@gmail.com>
2022-04-19 00:18:51 +0200

    load change commands from backend

--- loadChangesFromFile.js
+++ loadChangesFromFile.js
@@ -6,144 +6,147 @@ function loadChangesFromFile(event) {
     var reader = new FileReader();
     reader.onload = function (e) {
         changes = e.target.result.split(/\r?\n|\r/);
-        const commandRegex = /(?<Operand>([a-z]+)( |\t)|(\+|\=|\-)( |\t)?)(?<Value>(\".+\")|(\{.+\})|(.+?))( |\t)(?<Stat_Name>.+)/i;
-        let ignore = false;
-        let currentSelection = "";
-        for (changeCommandIndex = 0; changeCommandIndex < changes.length; changeCommandIndex++) {
-            const changeCommand = changes[changeCommandIndex].trim();
-            //comment
-            if (changeCommand[0] == '#' || changeCommand.length == 0 || ignore) {
-                if (!ignore) {
-                    if (changeCommand == "###") {
-                        let spn = addChangeCommandWithColors([changeCommand], ["#5E5E5E"]);
-                        spn[0].style.fontStyle = "italic";
-                        ignore = true;
-                    } else {
-                        addChangeCommandWithColors([changeCommand], ["grey"]);
-                    }
-                    continue;
-                } else {
-                    if (changeCommand.toLowerCase() == "# END".toLowerCase())
-                        ignore = false;
-                    let spn = addChangeCommandWithColors([changeCommand], ["#5E5E5E"]);
-                    spn[0].style.fontStyle = "italic";
-                    continue;
-                }
-
-            }
-            //sync
-            else if (changeCommand.includes("sync")) {
-
-                if (changeCommand.includes("<")) {
-                    //If first part of current Selection is not the name of some nation
-                    if (Nations[currentSelection.split(/\./gi)[0]] === 'undefined') {
-                        alert("You tried to run sync on a specific nation, but no nation is selected. This Operations was aborted");
-                        continue;
-                    }
-                    gameStats.evaluateNations();
-                    syncNation(correctAndSynonymCheck(currentSelection).split(/\./gi)[0]);
-                } else {
-                    gameStats.evaluateNations();
-                    syncNations();
-                }
-                let spn = addChangeCommandWithColors([changeCommand], ["MediumSpringGreen"]);
-                spn[0].style.fontWeight = "bold";
-            }
-            //Selection and deselections
-            else if (changeCommand[0] == '>' || changeCommand[0] == '<') {
-                let cc = changeCommand;
-                let cutback = function (str) {
-                    let index = str.slice(1).search(/\<|\>/) + 1;
-                    if (index == 0) return ""
-                    return str.slice(index)
-                }
-                while (cc.length > 0) {
-                    cc.trim();
-                    //selection
-                    if (cc[0] == '>') {
-                        let arg = cc.slice(1);
-                        let index = arg.search(/\<|\>/);
-                        let selection;
-                        if (index == -1)
-                            selection = arg.trim();
-                        else
-                            selection = arg.slice(0, index).trim();
-                        currentSelection += "." + selection;
-                    }
-                    //deselection
-                    else if (cc[0] == '<') {
-
-                        if (cc.slice(1, 4) == "...") {
-                            currentSelection = "";
-                        } else {
-                            if (currentSelection.includes("."))
-                                currentSelection = currentSelection.slice(0, currentSelection.lastIndexOf("."));
-                            else
-                                currentSelection = "";
-                        }
-                    }
-                    cc = cutback(cc);
-                }
-                addChangeCommandWithColors([changeCommand], ["dodgerBlue"]);
-            }
-            //Creation
-            else if (changeCommand.slice(0, 2) == "+>") {
-                let arg = changeCommand.slice(2).trim();
-                let objectClass = "Object";
-                if (/^\.Nations$/.test(currentSelection)) objectClass = "Nation";
-                if (/^\.(Cultures|Religions)$/.test(currentSelection)) objectClass = "SocialBehaviour";
-                if (/^\.Nations\..+\.(Culture|Religion)Groups$/.test(currentSelection)) objectClass = "SocialBehaviourGroup";
-                if (/^\.Nations\..+\.Climates$/.test(currentSelection)) objectClass = "Climate";
-                if (/^\.(Cultures|Religions)\..+\.Opinions$/.test(currentSelection)) objectClass = "Opinion";
-                if (/^\.Trades$/.test(currentSelection)) objectClass = "Trade";
-                if (arg.includes('=')) {
-                    let newName = arg.slice(0, arg.indexOf('=')).trim();
-                    let oldName = arg.slice(arg.indexOf('=') + 1).trim();
-                    gameStats.Nations[oldName].evaluateNation();
-                    (new Function(`\
-                    gameStats${currentSelection}.${newName} = new ${objectClass}("${newName}");\
-                    /* Copy all property values from old to new */\
-                    for (const propertyName in gameStats${currentSelection}.${oldName}) {\
-                        if(propertyName == "GovernmentName") continue;\
-                        const propertyToCopy = gameStats${currentSelection}.${oldName}[propertyName];\
-                        gameStats${currentSelection}.${newName}[propertyName] = JSON.parse(JSON.stringify(propertyToCopy));\
-                    }`))();
-
-                } else {
-                    (new Function(`gameStats${currentSelection}.${arg} = new ${objectClass}("${arg}");`))();
-                }
-                addChangeCommandWithColors([changeCommand], ["magenta"]);
-            }
-            //normal commands
-            else {
-                commandParameters = [];
-                //If 2 or more instances of tabulator in the string
-                if (changeCommand.replace(/[^\t]/g, "").length >= 2) {
-                    commandParameters = changeCommand.split("\t");
-                }
-                else {
-                    let match = commandRegex.exec(changeCommand);
-                    if (!commandRegex.test(changeCommand)) {
-                        alert("At line " + (changeCommandIndex + 1) + "\r\n\r\nA command wasn't understood:\r\n" + changeCommand + "\r\n Aborting.");
-                        continue;
-                    }
-                    commandParameters[0] = match.groups.Operand.trim();
-                    commandParameters[1] = match.groups.Value;
-
-                    commandParameters[2] = match.groups.Stat_Name;
-                }
-                normalCommand(correctAndSynonymCheck(currentSelection + "." + commandParameters[2]));
-            }
-        }
-
-        gameStats.evaluateNations();
-        updateDropdownSelection();
-        currentNationName = Object.keys(gameStats.Nations)[0];
-        createNationSheet(currentNationName);
-
+        loadChangesFromContent(changes);
     };
 
     reader.readAsText(file);
+}
+
+function loadChangesFromContent(changes){
+    const commandRegex = /(?<Operand>([a-z]+)( |\t)|(\+|\=|\-)( |\t)?)(?<Value>(\".+\")|(\{.+\})|(.+?))( |\t)(?<Stat_Name>.+)/i;
+    let ignore = false;
+    let currentSelection = "";
+    for (changeCommandIndex = 0; changeCommandIndex < changes.length; changeCommandIndex++) {
+        const changeCommand = changes[changeCommandIndex].trim();
+        //comment
+        if (changeCommand[0] == '#' || changeCommand.length == 0 || ignore) {
+            if (!ignore) {
+                if (changeCommand == "###") {
+                    let spn = addChangeCommandWithColors([changeCommand], ["#5E5E5E"]);
+                    spn[0].style.fontStyle = "italic";
+                    ignore = true;
+                } else {
+                    addChangeCommandWithColors([changeCommand], ["grey"]);
+                }
+                continue;
+            } else {
+                if (changeCommand.toLowerCase() == "# END".toLowerCase())
+                    ignore = false;
+                let spn = addChangeCommandWithColors([changeCommand], ["#5E5E5E"]);
+                spn[0].style.fontStyle = "italic";
+                continue;
+            }
+
+        }
+        //sync
+        else if (changeCommand.includes("sync")) {
+
+            if (changeCommand.includes("<")) {
+                //If first part of current Selection is not the name of some nation
+                if (Nations[currentSelection.split(/\./gi)[0]] === 'undefined') {
+                    alert("You tried to run sync on a specific nation, but no nation is selected. This Operations was aborted");
+                    continue;
+                }
+                gameStats.evaluateNations();
+                syncNation(correctAndSynonymCheck(currentSelection).split(/\./gi)[0]);
+            } else {
+                gameStats.evaluateNations();
+                syncNations();
+            }
+            let spn = addChangeCommandWithColors([changeCommand], ["MediumSpringGreen"]);
+            spn[0].style.fontWeight = "bold";
+        }
+        //Selection and deselections
+        else if (changeCommand[0] == '>' || changeCommand[0] == '<') {
+            let cc = changeCommand;
+            let cutback = function (str) {
+                let index = str.slice(1).search(/\<|\>/) + 1;
+                if (index == 0) return ""
+                return str.slice(index)
+            }
+            while (cc.length > 0) {
+                cc.trim();
+                //selection
+                if (cc[0] == '>') {
+                    let arg = cc.slice(1);
+                    let index = arg.search(/\<|\>/);
+                    let selection;
+                    if (index == -1)
+                        selection = arg.trim();
+                    else
+                        selection = arg.slice(0, index).trim();
+                    currentSelection += "." + selection;
+                }
+                //deselection
+                else if (cc[0] == '<') {
+
+                    if (cc.slice(1, 4) == "...") {
+                        currentSelection = "";
+                    } else {
+                        if (currentSelection.includes("."))
+                            currentSelection = currentSelection.slice(0, currentSelection.lastIndexOf("."));
+                        else
+                            currentSelection = "";
+                    }
+                }
+                cc = cutback(cc);
+            }
+            addChangeCommandWithColors([changeCommand], ["dodgerBlue"]);
+        }
+        //Creation
+        else if (changeCommand.slice(0, 2) == "+>") {
+            let arg = changeCommand.slice(2).trim();
+            let objectClass = "Object";
+            if (/^\.Nations$/.test(currentSelection)) objectClass = "Nation";
+            if (/^\.(Cultures|Religions)$/.test(currentSelection)) objectClass = "SocialBehaviour";
+            if (/^\.Nations\..+\.(Culture|Religion)Groups$/.test(currentSelection)) objectClass = "SocialBehaviourGroup";
+            if (/^\.Nations\..+\.Climates$/.test(currentSelection)) objectClass = "Climate";
+            if (/^\.(Cultures|Religions)\..+\.Opinions$/.test(currentSelection)) objectClass = "Opinion";
+            if (/^\.Trades$/.test(currentSelection)) objectClass = "Trade";
+            if (arg.includes('=')) {
+                let newName = arg.slice(0, arg.indexOf('=')).trim();
+                let oldName = arg.slice(arg.indexOf('=') + 1).trim();
+                gameStats.Nations[oldName].evaluateNation();
+                (new Function(`\
+                gameStats${currentSelection}.${newName} = new ${objectClass}("${newName}");\
+                /* Copy all property values from old to new */\
+                for (const propertyName in gameStats${currentSelection}.${oldName}) {\
+                    if(propertyName == "GovernmentName") continue;\
+                    const propertyToCopy = gameStats${currentSelection}.${oldName}[propertyName];\
+                    gameStats${currentSelection}.${newName}[propertyName] = JSON.parse(JSON.stringify(propertyToCopy));\
+                }`))();
+
+            } else {
+                (new Function(`gameStats${currentSelection}.${arg} = new ${objectClass}("${arg}");`))();
+            }
+            addChangeCommandWithColors([changeCommand], ["magenta"]);
+        }
+        //normal commands
+        else {
+            commandParameters = [];
+            //If 2 or more instances of tabulator in the string
+            if (changeCommand.replace(/[^\t]/g, "").length >= 2) {
+                commandParameters = changeCommand.split("\t");
+            }
+            else {
+                let match = commandRegex.exec(changeCommand);
+                if (!commandRegex.test(changeCommand)) {
+                    alert("At line " + (changeCommandIndex + 1) + "\r\n\r\nA command wasn't understood:\r\n" + changeCommand + "\r\n Aborting.");
+                    continue;
+                }
+                commandParameters[0] = match.groups.Operand.trim();
+                commandParameters[1] = match.groups.Value;
+
+                commandParameters[2] = match.groups.Stat_Name;
+            }
+            normalCommand(correctAndSynonymCheck(currentSelection + "." + commandParameters[2]));
+        }
+    }
+
+    gameStats.evaluateNations();
+    updateDropdownSelection();
+    currentNationName = Object.keys(gameStats.Nations)[0];
+    createNationSheet(currentNationName);
 }
 
 function syncNation(nationName) {
