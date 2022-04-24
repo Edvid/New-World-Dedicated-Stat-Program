6dae00567aadb1f9214b9dd467eb8fd45378ff62
Edvid <spacemastr@gmail.com>
2022-04-24 19:03:54 +0200

    printRLEfromImage python file for converting images to RLEs
    Slightly faulty as of now

---
+++ RLEs/printRLEfromImage.py
@@ -0,0 +1,28 @@
+from PIL import Image
+img = Image.open('../../cleanblankmap.png', 'r')
+
+pix_vals = list(img.getdata())
+
+counter = 0
+last_pixel_value = pix_vals[0]
+symbols = [' ', '.', '-', '@']
+uniqueColorsFound = [];
+
+
+
+for pixel_value in pix_vals:
+    if(pixel_value == last_pixel_value):
+        counter += 1
+    else:
+        print(counter, end="")
+        index = 0
+        try:
+            index = uniqueColorsFound.index(pixel_value)
+        except ValueError:
+            index = len(uniqueColorsFound)
+            uniqueColorsFound.append(pixel_value)
+
+        print(symbols[index], end="")
+
+        counter = 1
+        last_pixel_value = pixel_value
