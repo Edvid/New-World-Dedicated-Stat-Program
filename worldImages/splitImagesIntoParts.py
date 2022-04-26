90d9ef83c545e94b31e3051480c984186d579875
Edvid <spacemastr@gmail.com>
2022-04-26 23:35:17 +0200

    script might work in theory
    but as it is now, there's a MemoryError

--- worldImages/splitImagesIntoParts.py
+++ worldImages/splitImagesIntoParts.py
@@ -12,27 +12,39 @@ HEIGHT = 3363
 
 imgs = []
 
-pix_vals = list(img.getdata())
+pix_vals_raw = list(img.getdata())
+
+pix_vals = [(0,0,0,0)]*WIDTH*HEIGHT
+
+counter = 0
+tempTupleArray = [0,0,0,0]
+for raw in pix_vals_raw:
+    tempTupleArray[counter % 4] = raw
+    if((counter - 1) % 4 == 0): 
+        pix_vals[math.floor(counter / 4)] = (tempTupleArray[0], tempTupleArray[1], tempTupleArray[2], tempTupleArray[3])
+    counter += 1
 
 counter = 0
 last_pixel_value = pix_vals[0]
 
 uniqueColorsFound = [];
 
-
 for pixel_value in pix_vals:
     if pixel_value in uniqueColorsFound:
-        imgs[uniqueColorsFound.index(pixel_value)][math.floor(counter / WIDTH)][counter % WIDTH] = pixel_value
+        imgs[uniqueColorsFound.index(pixel_value)][counter] = pixel_value
     else:
-        imgs.append([[0]*WIDTH]*HEIGHT)
-        
+        print("New color! Length:" + str(len(uniqueColorsFound)))
+        imgs.append([(0,0,0,0)]*WIDTH*HEIGHT)
         uniqueColorsFound.append(pixel_value)
         last_pixel_value = pixel_value
-        imgs[uniqueColorsFound.index(pixel_value)][math.floor(counter / WIDTH)][counter % WIDTH] = pixel_value
+        imgs[uniqueColorsFound.index(pixel_value)][counter] = pixel_value
     counter += 1
 
 counter = 0
 for im in imgs:
-    Image.new('RBGA', (WIDTH, HEIGHT)).putdata(im).show().save(name + "/Split_" + counter + ".png")
+    curimg = Image.new('RGBA', (WIDTH, HEIGHT))
+    curimg.putdata(im)
+    """ curimg.show() """
+    curimg.save(name + "/Split_" + str(counter) + ".png")
     counter += 1
 
