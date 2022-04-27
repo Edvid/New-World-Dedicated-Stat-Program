fc382492378ca3c0fad990cc49fbcb1474001588
Edvid <spacemastr@gmail.com>
2022-04-27 02:33:44 +0200

    python script now actually creates images
    They are very buggy still tho

--- worldImages/splitImagesIntoParts.py
+++ worldImages/splitImagesIntoParts.py
@@ -5,46 +5,46 @@ import math
 from numpy import unicode_
 
 name = "tradeZones"
-img = Image.open(name + "/" + name + '.png', 'r')
+img = Image.open(name + "/" + name + '.png', 'r').convert("RGBA")
 WIDTH = 8392
 HEIGHT = 3363
 
 
 imgs = []
 
-pix_vals_raw = list(img.getdata())
+pix_vals = list(img.getdata())
 
-pix_vals = [(0,0,0,0)]*WIDTH*HEIGHT
-
-counter = 0
-tempTupleArray = [0,0,0,0]
-for raw in pix_vals_raw:
-    tempTupleArray[counter % 4] = raw
-    if((counter - 1) % 4 == 0): 
-        pix_vals[math.floor(counter / 4)] = (tempTupleArray[0], tempTupleArray[1], tempTupleArray[2], tempTupleArray[3])
-    counter += 1
-
-counter = 0
 last_pixel_value = pix_vals[0]
 
 uniqueColorsFound = [];
 
+usedBits = 16
+
+counter = 0
 for pixel_value in pix_vals:
     if pixel_value in uniqueColorsFound:
-        imgs[uniqueColorsFound.index(pixel_value)][counter] = pixel_value
+        if len(imgs[uniqueColorsFound.index(pixel_value)])*usedBits <= counter:
+            print(counter)
+        imgs[uniqueColorsFound.index(pixel_value)][counter//usedBits] += 1**(counter%usedBits)
     else:
-        print("New color! Length:" + str(len(uniqueColorsFound)))
-        imgs.append([(0,0,0,0)]*WIDTH*HEIGHT)
+        print("New color!: " + str(pixel_value) + ". Length: " + str(len(uniqueColorsFound)))
+        imgs.append([0]*math.ceil(WIDTH*HEIGHT/usedBits))
         uniqueColorsFound.append(pixel_value)
         last_pixel_value = pixel_value
-        imgs[uniqueColorsFound.index(pixel_value)][counter] = pixel_value
+        imgs[uniqueColorsFound.index(pixel_value)][counter//usedBits] = 1**(counter%usedBits)
     counter += 1
 
 counter = 0
 for im in imgs:
+
+    im_correct = []
+    for i in range(WIDTH*HEIGHT):
+            im_correct.append(((0, 0, 0, 0), (255, 255, 255, 255))[im[i//usedBits] & 1**(i%usedBits)])
+
     curimg = Image.new('RGBA', (WIDTH, HEIGHT))
-    curimg.putdata(im)
+    curimg.putdata(im_correct)
     """ curimg.show() """
     curimg.save(name + "/Split_" + str(counter) + ".png")
+    print("saving image: " + str(counter))
     counter += 1
 
