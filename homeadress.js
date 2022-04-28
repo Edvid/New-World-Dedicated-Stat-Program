6392007d53017ac112fb6b93cb5737f1130026a7
Edvid <spacemastr@gmail.com>
2022-04-28 14:28:52 +0200

    changed home_address to non local

--- homeadress.js
+++ homeadress.js
@@ -2,5 +2,7 @@
 //local host by typing "http-server" in a terminal after installing it
 //install by "npm install http-server -g"
 //example of what the string below should be for local testing is "http://127.0.0.1:8080/"
-const HOME_ADDRESS = "http://127.0.0.1:8080/";
-//const HOME_ADDRESS = "https://raw.githubusercontent.com/Edvid/New-World-Dedicated-Stat-Program/main/";
+
+
+//const HOME_ADDRESS = "http://127.0.0.1:8080/";
+const HOME_ADDRESS = "https://raw.githubusercontent.com/Edvid/New-World-Dedicated-Stat-Program/main/";
