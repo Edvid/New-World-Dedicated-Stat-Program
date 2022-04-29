d49dcf318178e74de1e5ada3a78316dd8d31d7d8
Edvid <spacemastr@gmail.com>
2022-04-29 08:38:09 +0200

    another fix to home address

--- homeadress.js
+++ homeadress.js
@@ -1,8 +1,8 @@
-//when downloaded for test. Replace the URL with a localhost URL
-//local host by typing "http-server" in a terminal after installing it
-//install by "npm install http-server -g"
-//example of what the string below should be for local testing is "http://127.0.0.1:8080/"
 
+let REPO_NAME = "";
 
-//const HOME_ADDRESS = "http://127.0.0.1:8080/";
-const HOME_ADDRESS = "/docs/assets/";
+//disable this line for local testing
+REPO_NAME = "/New-World-Dedicated-Stat-Program/"
+//
+
+const HOME_ADDRESS = REPO_NAME + "/docs/assets/";
