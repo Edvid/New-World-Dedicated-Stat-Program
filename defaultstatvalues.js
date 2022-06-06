727fcc73525d2f9d492f575712463e6f9e3c8e3f
Edvid <spacemastr@gmail.com>
2022-06-07 01:28:16 +0200

    Added default Values

---
+++ defaultstatvalues.js
@@ -0,0 +1,55 @@
+let defaultStatValues = {
+    Isolation: 0.50,
+    ResearchSpending: 0.05,
+    Health: 0.50,
+    EducationEfficiency: 0.50,
+    AdministrativeEfficiency: 0.50,
+    Propaganda: 0.25,
+    SocialSpending: 0.25,
+    Absolutism: 1.00,
+    PopulationControl: 0.50,
+    BirthControl: 0.50,
+    LocalTrade: 0.50,
+    Merchantilism: 0.05,
+    SpyQuality: 0.05,
+    NobleInfluence: 0.05,
+    ClergyInfluence: 0.05,
+    BurghersInfluence: 0.05,
+    NobleLoyalty: 0.05,
+    ClergyLoyalty: 0.05,
+    BurghersLoyalty: 0.05,
+    Workforces: 0.005,
+    MiningEfficiency: 0.2, //uncertain if correct number
+    Coal: 0.75, //uncertain if correct number
+    Sulphur: 0.75, // -||-
+    Cotton: 0.75, // -||-
+    Gold: 0.75, // -||-
+    Iron: 0.75, // ...
+    Tea: 0.75,
+    Silk: 0.75,
+    Spice: 0.75,
+    Wool: 0.75,
+    Coffee: 0.75,
+    Fur: 0.75,
+    Diamond: 0.75,
+    Silver: 0.75,
+    Copper: 0.75,
+    Ivory: 0.75,
+    Cocoa: 0.75,
+    Tobacco: 0.75,
+    Sugar: 0.75, // ...
+    ExoticFruit: 0.75, // -||-
+    AgriculturalSubsidies: 0.05,
+    AgricultureInfrastructure: 0.05,
+    AgricultureAdvancements: 0.05,
+    StockingCapabilities: 0.05,
+    ResearchEffectiveness: 0.05,
+    CulturalProsperity: 0.05,
+    CommanderFreedom: 0.25,
+    ArmyWages: 0.05,
+    TrainingQuality: 0.05,
+    MilitaryTactics: 0.05,
+    MilitaryDiscipline: 0.005,
+    NavalImprovements: 0.05,
+    TradeInfluences: 0.5
+}
