7a77e89d161bf272d7292180bfe3be012b991861
Edvid <spacemastr@gmail.com>
2022-08-08 19:00:41 +0200

    page made

--- shared/header.js
+++ shared/header.js
@@ -1,18 +1,22 @@
 let headerElement = document.querySelector("header");
+
 let homelink = document.createElement("button");
 let nationslink = document.createElement("button");
 let tradezonelink = document.createElement("button");
 let alltradeslink = document.createElement("button");
+let mapccflink = document.createElement("button");
 
 headerElement.appendChild(homelink);
 headerElement.appendChild(nationslink);
 headerElement.appendChild(tradezonelink);
 headerElement.appendChild(alltradeslink);
+headerElement.appendChild(mapccflink);
 
 homelink.innerText = "home";
 nationslink.innerText = "all nations";
 tradezonelink.innerText = "trade zone map";
 alltradeslink.innerText = "all trades";
+mapccflink.innerText = "map ccf calculations";
 
 homelink.onclick = function(){
     document.location.href = "./";
@@ -28,5 +32,9 @@ nationslink.onclick = function(){
 
 alltradeslink.onclick = function(){
     document.location.href = "./alltrades";
+};
+
+mapccflink.onclick = function(){
+    document.location.href = "./mapccfcalculations";
 };
 
