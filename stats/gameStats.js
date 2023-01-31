08d8dd2b770dc51adc787a9c1f89558740850a2a
Edvid <spacemastr@gmail.com>
2023-01-31 18:26:41 +0100

    Climates constructor also has colors

--- stats/gameStats.js
+++ stats/gameStats.js
@@ -548,7 +548,7 @@ class Nation {
     this.Flag = "";
     this.Color = false;
     this.ReligionGroups = {
-      "Pagan": {
+      Pagan: {
         Points: 100
       }
     };
@@ -1054,44 +1054,61 @@ class Stats{
     }
 
     this.Climates = {
-      PolarDesert: {
+      Ocean: {
         ClimateScore: 0,
+        Color: "103c6d"
+      },
+      PolarDesert: {
+          ClimateScore: 0,
+          Color: "808080"
       },
       TaigaAndTundra: {
-        ClimateScore: 0.25,
+          ClimateScore: 0.25,
+          Color: "004a7f"
       },
       MontaneForest: {
-        ClimateScore: 0.6,
+          ClimateScore: 0.6,
+          Color: "ffac7f"
       },
       Medditereanian: {
-        ClimateScore: 0.85,
+          ClimateScore: 0.85,
+          Color: "ff6a00"
       },
       Arid: {
-        ClimateScore: 0.65,
+          ClimateScore: 0.65,
+          Color: "7f3300"
       },
       Steppe: {
-        ClimateScore: 0.75,
+          ClimateScore: 0.75,
+          Color: "c8ff7c"
       },
       Moderate: {
-        ClimateScore: 1,
+          ClimateScore: 1,
+          Color: "4cff00"
       },
       SubTropical: {
-        ClimateScore: 0.75,
+          ClimateScore: 0.75,
+          Color: "5b7f00"
       },
       Tropical: {
-        ClimateScore: 0.6,
+          ClimateScore: 0.6,
+          Color: "008010"
       },
       Savanna: {
-        ClimateScore: 0.65,
+          ClimateScore: 0.65,
+          Color: "c1bd3e"
       },
       Mountainous: {
-        ClimateScore: 0.35,
+          ClimateScore: 0.35,
+          Color: "ff0000"
       },
       Desert: {
-        ClimateScore: 0.05,
+          ClimateScore: 0.05,
+          Color: "fffb99"
       },
       CoastalDesert: {
-        ClimateScore: 0.35
+          ClimateScore: 0.35,
+          Color: "ffd802"
       }
     };
 
