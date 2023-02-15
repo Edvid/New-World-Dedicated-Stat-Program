5deda131eb6766b1b386c25ed1568da0a2cf85a5
Edvid <spacemastr@gmail.com>
2023-02-15 16:57:26 +0100

    rewritten getStatType again

--- shared/utility.js
+++ shared/utility.js
@@ -611,7 +611,9 @@ function getStatType(selection){
         if (foundStatType != null) return;
         for(let i = 0; i < StatTypes[statType].length; i++){
             const endCompare = StatTypes[statType][i];
-            if(selection.endsWith(endCompare)) {
+            const testString = endCompare + '$';
+            
+            if(new RegExp(testString).test(selection)) {
                 foundStatType = statType.split(/Stats/)[0].replace(/([a-z])([A-Z])/, "$1 $2");
                 break;
             } 
