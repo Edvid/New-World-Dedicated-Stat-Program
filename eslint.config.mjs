3acd4db7592bd1254d484a1d19f59e308eb94947
Edvid <spacemastr@gmail.com>
2025-08-08 06:45:24 +0200

    fix(eslint): allow browser globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,11 +3,19 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
+import globals from "globals";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
   tseslint.configs.stylistic,
   eslintConfigPrettier
 );
