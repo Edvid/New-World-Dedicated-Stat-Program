893eec7fa16fb4f4070b32bbbc82f474765fe3c0
Edvid <spacemastr@gmail.com>
2025-09-19 09:07:09 +0200

    chore(synonyms.js): changed whitespace. Looks better

--- ccfassist/synonyms.js
+++ ccfassist/synonyms.js
@@ -1,545 +1,558 @@
 export let Synonyms = {
-    AdministrativeEfficiency: [
-        "adm",
-        "admeff",
-        "admineff"
-    ],
-    AdministrationSize: [
-        "AdmSize"
-    ],
-    PopulationControl: [
-        "PopControl",
-        "popctrl",
-        "populationctrl"
-    ],
-    TradeImprovements: [
-        "TradeImprov"
-    ],
-    OverallImprovements: [
-        "OverallImprov"
-    ],
-    MeleeImprovements: [
-        "MeleeImprov"
-    ],
-    IrregularImprovements: [
-        "IrregularImprov"
-    ],
-    RangedImprovements: [
-        "RangedImprov"
-    ],
-    FirearmImprovements: [
-        "FirearmImprov"
-    ],
-    CavalryImprovements: [
-        "CavalryImprov",
-        "CavImprov",
-    ],
-    SiegeImprovements: [
-        "SiegeImprov"
-    ],
-    ArtilleryImprovements: [
-        "ArtilleryImprov"
-    ],
-    LightShipImprovements: [
-        "LightShipImprov"
-    ],
-    MediumShipImprovements: [
-        "MediumShipImprov"
-    ],
-    HeavyShipImprovements: [
-        "HeavyShipImprov"
-    ],
-    Prosperity: [
-        "pros",
-        "prosp",
-        "qualityoflife",
-        "qualoflife",
-        "qualofl",
-        "qual",
-		"qof",
-		"qf",
-		"hdi"
-    ],
+  AdministrativeEfficiency: [
+    "adm",
+    "admeff",
+    "admineff"
+  ],
 
-	SocialSpending: [
-		"socspending",
-		"socspend",
-		"socialspend"
-	],
+  AdministrationSize: [
+    "AdmSize"
+  ],
 
-	ReligionGroups : [
-		"relgr",
-		"rg"
-	],
+  PopulationControl: [
+    "PopControl",
+    "popctrl",
+    "populationctrl"
+  ],
 
-	CultureGroups: [
-		"cultgr",
-		"culgr",
-		"cg"
-	],
-	
-    ReligionRepresentedAtGovernmentLevel: [
-        "primaryreligion",
-        "primrel",
-        "relrep",
-		"governmentreligion",
-		"govrel",
-		"mainreligion",
-		"mainrel"
-		
-    ],
-	
-    CultureRepresentedAtGovernmentLevel: [
-        "primaryculture",
-        "primcul",
-        "primcult",
-        "culrep",
-        "cultrep",
-		"governmentculture",
-		"govcult",
-		"mainculture",
-		"maincult"
-    ],
-	
-    Pixels: [
-        "points",
-		"size"
-    ],
-	
-	Population: [
-		"pop",
-		"populace"
-	],
-	
-	Health: [
-		"hygiene",
-		"healthcare"
-	],
-	
-	LiteracyPercent: [
-		"literacy"
-	],
-	
-	HigherEducation: [
-		"higheredu",
-		"highedu"
-	],
-	
-	EducationEfficiency: [
-		"edu",
-		"edueff",
-		"eduefficiency",
-		"education"
-	],
-	
-	BirthControl: [
-		"birthctrl",
-		"popgrowthcontrol",
-		"populationgrowthcontrol",
-		"popgrowthctrl"
-	],
-	
-	Budget: [
-		"money",
-		"treasury"
-	],
-	
-	AristocratInfluence: [
-		"Aristocracyinfluence",
-		"Aristocratinf",
-		"Aristocracyinf"
-	],
-	
-	ClergyInfluence: [
-		"clergyinf"
-	],
-	
-	BurgousieInfluence: [
-		"Burgousieinf"
-	],
-	
-	AristocratLoyalty: [
-		"Aristocracyloyalty",
-		"Aristocratloyalty",
-		"Aristocratlg"
-	],
-	
-	ClergyLoyalty: [
-		"clergyloyalty",
-		"clergylg"
-	],
-	
-	BurgousieLoyalty: [
-		"Burgousieloyalty",
-		"Burgousieloyalty",
-		"Burgousielg",
-		"Burgousielg",
-	],
+  TradeImprovements: [
+    "TradeImprov"
+  ],
 
-	Levies: [
-		"levy",
-		"levie"
-	],
-	
-	LightInfantry: [
-		"lightinf",
-		"linf"
-	],
-	
-	HeavyInfantry: [
-		"heavyinf",
-		"hinf"
-	],
-	
-	Crossbowmen: [
-		"crossbows",
-		"crossbow",
-		"crossbowman"
-	],
-	
-	LightCavalry: [
-		"lightcav",
-		"lcav"
-	],
-	
-	HeavyCavalry: [
-		"heavycav",
-		"hcav"
-	],
-	
-	EliteInfantry: [
-		"eliteinf",
-		"einf"
-	],
-	
-	EliteCavalry: [
-		"elitecav",
-		"ecav"
-	],
-	
-	Musketeers: [
-		"musketeer",
-		"musketmen",
-		"musketman"
-	],
+  OverallImprovements: [
+    "OverallImprov"
+  ],
 
-	HandCannoneers: [
-		"Handcannoneer",
-		"Handcannonmen",
-	],
-	
-	SiegeEquipment: [
-		"siegeeq"
-	],
-	
-	LargeSiegeEquipment: [
-		"largesiegeeq"
-	],
-	
-	Cannons: [
-		"artillery",
-		"art",
-		"arty"
-	],
-	
-	SmallForts: [
-		"smallfort"
-	],
-	
-	MediumForts: [
-		"mediumfort"
-	],
-	
-	BigForts: [
-		"bigfort"
-	],
-	
-	HugeForts: [
-		"largeforts",
-		"hugefort",
-		"largefort"
-	],
+  MeleeImprovements: [
+    "MeleeImprov"
+  ],
 
-	LightShips: [
-		"lship",
-		"lightship",
-		"lships",
-	],
+  IrregularImprovements: [
+    "IrregularImprov"
+  ],
 
-	MediumShips: [
-		"mship",
-		"mships",
-		"medship",
-		"mediumship",
-		"medships",
-	],
+  RangedImprovements: [
+    "RangedImprov"
+  ],
 
-	HeavyShips: [
-		"hship",
-		"heavyship",
-		"hships",
-	],
-	
-	ExtraCityFortifications: [
-		"cityfortifications",
-		"citydefenses",
-		"extracitydefenses",
-		"citydefences",
-		"extracitydefences"
-	],
-	
-	CommanderFreedom: [
-		"officerfreedom",
-		"commanderautonomy",
-		"officerautonomy",
-		"armyautonomy"
-	],
-	
-	ArmyWages: [
-		"wages"
-	],
-	
-	MilitaryDiscipline: [
-		"discipline",
-		"discipline"
-	],
-	
-	NavyImprovements: [
-		"navyimprov",
-		"navyimprovement",
-		"navalimprovements",
-		"navalimprovement",
-		"navalimprov"
-	],
-	
-	Aristocracy: [
-		"Aristocrats",
-		"aristocracy"
-	],
-	
-	FarmingEfficiency: [
-		"farmingeff"
-	],
-	
-	MiningEfficiency: [
-		"miningeff"
-	],
-	
-	Spice: [
-		"spices"
-	],
-	
-	Fur: [
-		"furs"
-	],
-	
-	Diamond: [
-		"diamonds"
-	],
-	
-	ExoticFruit: [
-		"exofruit",
-		"exoticfruits",
-		"exofruits"
-	],
-	
-	ResearchEffectiveness: [
-		"researcheff"
-	],
-	
-	ResearchPoints: [
-		"rp"
-	],
-	
-	Technologies: [
-		"techs"
-	],
+  FirearmImprovements: [
+    "FirearmImprov"
+  ],
 
-	SocietalClass: [
-		"socclass",
-		"classes"
-	],
+  CavalryImprovements: [
+    "CavalryImprov",
+    "CavImprov",
+  ],
 
-	Lower: [
-		"l",
-		"low"
-	],
+  SiegeImprovements: [
+    "SiegeImprov"
+  ],
 
-	Medium: [
-		"m",
-		"mid"
-	],
+  ArtilleryImprovements: [
+    "ArtilleryImprov"
+  ],
 
-	High: [
-		"h"
-	],
-	
-	HighClassTax: [
-		"hightax",
-		"highertax"
-	],
-	
-	MediumClassTax: [
-		"mediumtax",
-		"middletax",
-		"medtax",
-		"midtax"
-	],
-	
-	LowerClassTax: [
-		"lowertax",
-		"lowtax"
-	],
-	
-	PublicDebtTaken: [
-		"debttaken",
-		"debt"
-	],
-	
-	CulturalProsperity: [
-		"cultureprosperity"
-	],
-	
-	CulturalPower: [
-		"cp"
-	],
-	
-	SpiceIncoming: [
-		"spicesincoming"
-	],
-	
-	SpiceOutgoing: [
-		"spicesoutgoing"
-	],
-	
-	FurIncoming: [
-		"fursincoming"
-	],
-	
-	FurOutgoing: [
-		"fursoutgoing"
-	],
-	
-	DiamondIncoming: [
-		"diamondsincoming"
-	],
-	
-	DiamondOutgoing: [
-		"diamondsoutgoing"
-	],
-	
-	ExoticFruitIncoming: [
-		"exoticfruitsincoming",
-		"exofruitincoming",
-		"exofruitsincoming"
-	],
-	
-	ExoticFruitOutgoing: [
-		"exoticfruitsoutgoing",
-		"exofruitoutgoing",
-		"exofruitsoutgoing"
-	],
-	
-	AgricultureSubsidies: [
-		"agrsubsidies",
-		"agriculturesubs",
-		"agrsubs",
-		"agrsub",
-		"agrisubs",
-		"agrisub",
-	],
-	
-	AgricultureInfrastructure: [
-		"agricultureinf",
-		"agricultureinfra",
-		"agrinf",
-		"agriinf",
-		"agrinfra"
-	],
-	
-	StockingCapabilities: [
-		"stockcap"
-	],
-	
-	AgricultureAdvancements: [
-		"agricultureadvancements",
-		"agricultureadv",
-		"agradvancements",
-		"agradv",
-		"agriadv",
-		"agricultureimprovements",
-		"agricultureimprov",
-		"agrimprovements",
-		"agrimprov"
-	],
-	
-	Food: [
-		"stock",
-		"stockedfood",
-		"foodstock"
-	],
-	
-	Casualties: [
-		"loses"
-	],
-	
-	TradingPoints: [
-		"tradep",
-		"tp"
-	],
-	
-	TradeInfluences: [
-		"TradeInfluence",
-		"TradeInf"
-	],
+  LightShipImprovements: [
+    "LightShipImprov"
+  ],
 
-	WesternMediterranean: [
-		"westernmed",
-		"westmed"
-	],
+  MediumShipImprovements: [
+    "MediumShipImprov"
+  ],
 
-	CentralMediterranean: [
-		"centralmed",
-		"middlemed",
-		"midmed"
-	],
+  HeavyShipImprovements: [
+    "HeavyShipImprov"
+  ],
 
-	EasternMediterranean: [
-		"easternmed",
-		"eastmed"
-	],
+  Prosperity: [
+    "pros",
+    "prosp",
+    "qualityoflife",
+    "qualoflife",
+    "qualofl",
+    "qual",
+    "qof",
+    "qf",
+    "hdi"
+  ],
 
-	EasternDanube: [
-		"eastdanube",
-		"eastdan"
-	],
+  SocialSpending: [
+    "socspending",
+    "socspend",
+    "socialspend"
+  ],
 
-	WesternDanube: [
-		"westdanube",
-		"westdan"
-	],
+  ReligionGroups : [
+    "relgr",
+    "rg"
+  ],
 
-	CaliforniaAndWestMexico:[
-		"california",
-		"westernmexico",
-		"mexico",
-		"calimex",
-	],
+  CultureGroups: [
+    "cultgr",
+    "culgr",
+    "cg"
+  ],
 
-	DonRiver:[
-		"don"
-	],
+  ReligionRepresentedAtGovernmentLevel: [
+    "primaryreligion",
+    "primrel",
+    "relrep",
+    "governmentreligion",
+    "govrel",
+    "mainreligion",
+    "mainrel"
+  ],
 
-	Yangtze: [
-		"Yangtzi",
-		"centralchina",
-		"midchina"
-	],
+  CultureRepresentedAtGovernmentLevel: [
+    "primaryculture",
+    "primcul",
+    "primcult",
+    "culrep",
+    "cultrep",
+    "governmentculture",
+    "govcult",
+    "mainculture",
+    "maincult"
+  ],
 
-	WesternSahara: [
-		"westsahara",
-		"westernafrica",
-		"westafrica"
-	]
+  Pixels: [
+    "points",
+    "size"
+  ],
 
+  Population: [
+    "pop",
+    "populace"
+  ],
+
+  Health: [
+    "hygiene",
+    "healthcare"
+  ],
+
+  LiteracyPercent: [
+    "literacy"
+  ],
+
+  HigherEducation: [
+    "higheredu",
+    "highedu"
+  ],
+
+  EducationEfficiency: [
+    "edu",
+    "edueff",
+    "eduefficiency",
+    "education"
+  ],
+
+  BirthControl: [
+    "birthctrl",
+    "popgrowthcontrol",
+    "populationgrowthcontrol",
+    "popgrowthctrl"
+  ],
+
+  Budget: [
+    "money",
+    "treasury"
+  ],
+
+  AristocratInfluence: [
+    "Aristocracyinfluence",
+    "Aristocratinf",
+    "Aristocracyinf"
+  ],
+
+  ClergyInfluence: [
+    "clergyinf"
+  ],
+
+  BurgousieInfluence: [
+    "Burgousieinf"
+  ],
+
+  AristocratLoyalty: [
+    "Aristocracyloyalty",
+    "Aristocratloyalty",
+    "Aristocratlg"
+  ],
+
+  ClergyLoyalty: [
+    "clergyloyalty",
+    "clergylg"
+  ],
+
+  BurgousieLoyalty: [
+    "Burgousieloyalty",
+    "Burgousieloyalty",
+    "Burgousielg",
+    "Burgousielg",
+  ],
+
+  Levies: [
+    "levy",
+    "levie"
+  ],
+
+  LightInfantry: [
+    "lightinf",
+    "linf"
+  ],
+
+  HeavyInfantry: [
+    "heavyinf",
+    "hinf"
+  ],
+
+  Crossbowmen: [
+    "crossbows",
+    "crossbow",
+    "crossbowman"
+  ],
+
+  LightCavalry: [
+    "lightcav",
+    "lcav"
+  ],
+
+  HeavyCavalry: [
+    "heavycav",
+    "hcav"
+  ],
+
+  EliteInfantry: [
+    "eliteinf",
+    "einf"
+  ],
+
+  EliteCavalry: [
+    "elitecav",
+    "ecav"
+  ],
+
+  Musketeers: [
+    "musketeer",
+    "musketmen",
+    "musketman"
+  ],
+
+  HandCannoneers: [
+    "Handcannoneer",
+    "Handcannonmen",
+  ],
+
+  SiegeEquipment: [
+    "siegeeq"
+  ],
+
+  LargeSiegeEquipment: [
+    "largesiegeeq"
+  ],
+
+  Cannons: [
+    "artillery",
+    "art",
+    "arty"
+  ],
+
+  SmallForts: [
+    "smallfort"
+  ],
+
+  MediumForts: [
+    "mediumfort"
+  ],
+
+  BigForts: [
+    "bigfort"
+  ],
+
+  HugeForts: [
+    "largeforts",
+    "hugefort",
+    "largefort"
+  ],
+
+  LightShips: [
+    "lship",
+    "lightship",
+    "lships",
+  ],
+
+  MediumShips: [
+    "mship",
+    "mships",
+    "medship",
+    "mediumship",
+    "medships",
+  ],
+
+  HeavyShips: [
+    "hship",
+    "heavyship",
+    "hships",
+  ],
+
+  ExtraCityFortifications: [
+    "cityfortifications",
+    "citydefenses",
+    "extracitydefenses",
+    "citydefences",
+    "extracitydefences"
+  ],
+
+  CommanderFreedom: [
+    "officerfreedom",
+    "commanderautonomy",
+    "officerautonomy",
+    "armyautonomy"
+  ],
+
+  ArmyWages: [
+    "wages"
+  ],
+
+  MilitaryDiscipline: [
+    "discipline",
+    "discipline"
+  ],
+
+  NavyImprovements: [
+    "navyimprov",
+    "navyimprovement",
+    "navalimprovements",
+    "navalimprovement",
+    "navalimprov"
+  ],
+
+  Aristocracy: [
+    "Aristocrats",
+    "aristocracy"
+  ],
+
+  FarmingEfficiency: [
+    "farmingeff"
+  ],
+
+  MiningEfficiency: [
+    "miningeff"
+  ],
+
+  Spice: [
+    "spices"
+  ],
+
+  Fur: [
+    "furs"
+  ],
+
+  Diamond: [
+    "diamonds"
+  ],
+
+  ExoticFruit: [
+    "exofruit",
+    "exoticfruits",
+    "exofruits"
+  ],
+
+  ResearchEffectiveness: [
+    "researcheff"
+  ],
+
+  ResearchPoints: [
+    "rp"
+  ],
+
+  Technologies: [
+    "techs"
+  ],
+
+  SocietalClass: [
+    "socclass",
+    "classes"
+  ],
+
+  Lower: [
+    "l",
+    "low"
+  ],
+
+  Medium: [
+    "m",
+    "mid"
+  ],
+
+  High: [
+    "h"
+  ],
+
+  HighClassTax: [
+    "hightax",
+    "highertax"
+  ],
+
+  MediumClassTax: [
+    "mediumtax",
+    "middletax",
+    "medtax",
+    "midtax"
+  ],
+
+  LowerClassTax: [
+    "lowertax",
+    "lowtax"
+  ],
+
+  PublicDebtTaken: [
+    "debttaken",
+    "debt"
+  ],
+
+  CulturalProsperity: [
+    "cultureprosperity"
+  ],
+
+  CulturalPower: [
+    "cp"
+  ],
+
+  SpiceIncoming: [
+    "spicesincoming"
+  ],
+
+  SpiceOutgoing: [
+    "spicesoutgoing"
+  ],
+
+  FurIncoming: [
+    "fursincoming"
+  ],
+
+  FurOutgoing: [
+    "fursoutgoing"
+  ],
+
+  DiamondIncoming: [
+    "diamondsincoming"
+  ],
+
+  DiamondOutgoing: [
+    "diamondsoutgoing"
+  ],
+
+  ExoticFruitIncoming: [
+    "exoticfruitsincoming",
+    "exofruitincoming",
+    "exofruitsincoming"
+  ],
+
+  ExoticFruitOutgoing: [
+    "exoticfruitsoutgoing",
+    "exofruitoutgoing",
+    "exofruitsoutgoing"
+  ],
+
+  AgricultureSubsidies: [
+    "agrsubsidies",
+    "agriculturesubs",
+    "agrsubs",
+    "agrsub",
+    "agrisubs",
+    "agrisub",
+  ],
+
+  AgricultureInfrastructure: [
+    "agricultureinf",
+    "agricultureinfra",
+    "agrinf",
+    "agriinf",
+    "agrinfra"
+  ],
+
+  StockingCapabilities: [
+    "stockcap"
+  ],
+
+  AgricultureAdvancements: [
+    "agricultureadvancements",
+    "agricultureadv",
+    "agradvancements",
+    "agradv",
+    "agriadv",
+    "agricultureimprovements",
+    "agricultureimprov",
+    "agrimprovements",
+    "agrimprov"
+  ],
+
+  Food: [
+    "stock",
+    "stockedfood",
+    "foodstock"
+  ],
+
+  Casualties: [
+    "loses"
+  ],
+
+  TradingPoints: [
+    "tradep",
+    "tp"
+  ],
+
+  TradeInfluences: [
+    "TradeInfluence",
+    "TradeInf"
+  ],
+
+  WesternMediterranean: [
+    "westernmed",
+    "westmed"
+  ],
+
+  CentralMediterranean: [
+    "centralmed",
+    "middlemed",
+    "midmed"
+  ],
+
+  EasternMediterranean: [
+    "easternmed",
+    "eastmed"
+  ],
+
+  EasternDanube: [
+    "eastdanube",
+    "eastdan"
+  ],
+
+  WesternDanube: [
+    "westdanube",
+    "westdan"
+  ],
+
+  CaliforniaAndWestMexico:[
+    "california",
+    "westernmexico",
+    "mexico",
+    "calimex",
+  ],
+
+  DonRiver:[
+    "don"
+  ],
+
+  Yangtze: [
+    "Yangtzi",
+    "centralchina",
+    "midchina"
+  ],
+
+  WesternSahara: [
+    "westsahara",
+    "westernafrica",
+    "westafrica"
+  ]
 }
