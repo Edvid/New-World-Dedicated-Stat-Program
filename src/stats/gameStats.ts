5032691bb76a9bf2ffa95eff852ed2d91ffb6bce
Edvid <spacemastr@gmail.com>
2025-10-02 04:01:00 +0200

    refactor(game_stats): climates in own file

--- src/stats/gameStats.ts
+++ src/stats/gameStats.ts
@@ -2,6 +2,7 @@ import { type RealReformCostTypes, type ReformsType } from "../utility/mapcalc/t
 import { realResourceTypes } from "../utility/game_stats/resources.js";
 import { startingReligionGroups, religions } from "../utility/game_stats/religions.js";
 import { type Trade, type TradeZonesType, type tradeInfluencesType, tradeZones } from "../utility/game_stats/trades.js";
+import { type Climates, climates } from "../utility/game_stats/climates.js";
 
 export class SocialBehaviour {
   Color = "000000";
@@ -10,11 +11,6 @@ export class SocialBehaviour {
 
 export class SocialBehaviourGroup {
   Points = 0;
-}
-
-export class WorldClimate{
-  Color;
-  ClimateScore;
 }
 
 export class NationClimate { 
@@ -89,64 +85,6 @@ interface productionSectors {
   HeavyIndustrySector: number;
 }
 
-const climates = {
-  Ocean: {
-    ClimateScore: 0,
-    Color: "103c6d"
-  },
-  PolarDesert: {
-    ClimateScore: 0.001,
-    Color: "808080"
-  },
-  TaigaAndTundra: {
-    ClimateScore: 0.25,
-    Color: "004a7f"
-  },
-  MontaneForest: {
-    ClimateScore: 0.6,
-    Color: "ffac7f"
-  },
-  Medditereanian: {
-    ClimateScore: 0.85,
-    Color: "ff6a00"
-  },
-  Arid: {
-    ClimateScore: 0.65,
-    Color: "7f3300"
-  },
-  Steppe: {
-    ClimateScore: 0.75,
-    Color: "c8ff7c"
-  },
-  Moderate: {
-    ClimateScore: 1,
-    Color: "4cff00"
-  },
-  SubTropical: {
-    ClimateScore: 0.75,
-    Color: "5b7f00"
-  },
-  Tropical: {
-    ClimateScore: 0.6,
-    Color: "008010"
-  },
-  Savanna: {
-    ClimateScore: 0.65,
-    Color: "c1bd3e"
-  },
-  Mountainous: {
-    ClimateScore: 0.35,
-    Color: "ff0000"
-  },
-  Desert: {
-    ClimateScore: 0.05,
-    Color: "fffb99"
-  },
-  CoastalDesert: {
-    ClimateScore: 0.35,
-    Color: "ffd802"
-  }
-}
 const fertilities = {
   Infertile: { Color: "ffffff", Score: 0.025 },
   BarelyFertile: { Color: "ff0000", Score: 0.15 },
@@ -1675,7 +1613,7 @@ export class Stats {
   EstateGenereal = estates;
   Trades: Record<string, Trade> = {};
   TradeZones: TradeZonesType = tradeZones;
-  Climates: Record<string, WorldClimate> = climates;
+  Climates: Climates = climates;
   Fertility: Record<string, {Color: string, Score: number}> = fertilities;
   UnitUpkeepCosts = unitUpkeepCosts;
   AdvancesPrerequisites = {};
